fc5a3ded14ea45287e5a408e129f534705f7e9b8
AKSHAYBINDU <akshaykumarkanad@gmail.com>
2023-10-10 13:41:29 +0530

    Book view completed.

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 //OPTION-2
 // app.use(
 //   cors({
-//     origin: "",
+//     origin: " http://localhost:5173/",
 //     methods: ["GET", "POST", "PUT", "DELETE"],
 //     allowedHeaders: ["Content-Type"],
 //   })
